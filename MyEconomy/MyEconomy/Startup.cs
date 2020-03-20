dc7c0d1839d9c281e3ed666cd85f11b40ba268c9
Gino Del Carmen <gino.delcarmen@viseo.com>
2020-03-20 16:53:15 +0100

    Created master branch

---
+++ MyEconomy/MyEconomy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyEconomy.Startup))]
+namespace MyEconomy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
